129fbbdc0ba1301ef263595184fdfc9dab8bcb70
ztan1463 <ztan1463@pc-3e74-0.cs.usyd.edu.au>
2016-07-28 17:46:15 +1000

    add all

---
+++ run_all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python pokeslack_usyd_all.py -u usydnoti -p pokeslack -l "Carslaw Building, Camperdown, New South Wales" -st 5 -r 2000 -sw "https://hooks.slack.com/services/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" -L zh_cn -pi ':'
+# -33.8890000 151.1855000
---
+++ run_rare.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python pokeslack_usyd_rare.py -u usydnoti -p pokeslack -l "Carslaw Building, Camperdown, New South Wales" -st 3 -r 1500 -sw "https://hooks.slack.com/services/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" -L zh_cn -pi ':'
